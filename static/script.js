7d0d0a4e3eef3633b0dfb8b4b5700a38d6043195
abc <abc@example.com>
2019-02-09 22:35:31 +1300

    basically done

--- static/script.js
+++ static/script.js
@@ -3,37 +3,37 @@ $(document).ready(function() {
   var pageNum = 1;
 
   function turnToPage(num, operation) {
-  //pageNum starts at 1
-  //Easier to display
-  if (operation === "+") {
-    if (pageNum += num <= pictures.length) {
-      pageNum += num;
+    //pageNum starts at 1
+    //Easier to display
+    if (operation === "+") {
+      if (pageNum += num <= pictures.length) {
+        pageNum += num;
+      }
+    } else if (operation === "-") {
+      if ((pageNum -= num >= 1)) {
+        pageNum -= num;
+      }
+    } else if (num && !operation) {
+      if (pageNum >= 1 && pageNum <= pictures.length) {
+        pageNum = num;
+      }
     }
-  } else if (operation === "-") {
-    if ((pageNum -= num >= 1)) {
-      pageNum -= num;
-    }
-  } else if (num && !operation) {
-    if (pageNum >= 1 && pageNum <= pictures.length) {
-      pageNum = num;
-    }
-  }
     $("#comicPage").attr("src", pictures[pageNum - 1]);
   }
 
-    turnToPage();
+  turnToPage();
 
-    $("#picture").click(function() {
-       turnToPage(1, "+");
-    });
+  $("#picture").click(function() {
+    turnToPage(1, "+");
+  });
 
-    $("#prevPage").click(function() {
-      turnToPage(1, "-");
-    });
+  $("#prevPage").click(function() {
+    turnToPage(1, "-");
+  });
 
 
-    $("#nextPage").click(function() {
-      turnToPage(1, "+");
-    });
+  $("#nextPage").click(function() {
+    turnToPage(1, "+");
+  });
 
 });
